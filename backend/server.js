09178cbe91c45d2f275914a6d71d5ccff7f2e1aa
mispranami <pranamiswarajindia@gmail.com>
2025-03-01 22:06:19 +0530

    Initial commit with Express server setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Hive News Platform API is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
